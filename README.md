78816571ed5017ad43fde438eb9b1e66bacc5cdc
Devin Ivy <devin@bigroomstudios.com>
2016-05-17 17:22:39 -0400

    First commit, mmmmm

---
+++ README.md
@@ -0,0 +1,5 @@
+# haute
+
+directory structure as code
+
+[![Build Status](https://travis-ci.org/devinivy/haute.svg?branch=master)](https://travis-ci.org/devinivy/haute) [![Coverage Status](https://coveralls.io/repos/devinivy/haute/badge.svg?branch=master&service=github)](https://coveralls.io/github/devinivy/haute?branch=master)
---
+++ test/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Load modules
+
+const Lab = require('lab');
+const Code = require('code');
+const Haute = require('..');
+
+// Test shortcuts
+
+const lab = exports.lab = Lab.script();
+const describe = lab.describe;
+const it = lab.it;
+const expect = Code.expect;
+
+const internals = {};
+
+describe('Haute', () => {
+
+    it('', (done) => {
+
+
+    });
+
+});
